bdeee5cfd597c288a41c1513382565a2ffe54f89
Louis Coussement <coussementlouis1@gmail.com>
2025-11-28 10:47:16 +0000

    My first website

--- app.py
+++ app.py
@@ -1,27 +1,4 @@
 import streamlit as st
-
-'''
-# TaxiFareModel front
-'''
-
-st.markdown('''
-Remember that there are several ways to output content into your web page...
-
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
-''')
-
-'''
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
-
-1. Let's ask for:
-- date and time
-- pickup longitude
-- pickup latitude
-- dropoff longitude
-- dropoff latitude
-- passenger count
-'''
-
 import datetime
 import requests
 
