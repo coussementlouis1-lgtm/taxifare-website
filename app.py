a82c61449420d5f4c4be183eb8d5782cfd5d0fb9
Louis Coussement <coussementlouis1@gmail.com>
2025-11-28 10:35:44 +0000

    My first website

---
+++ app.py
@@ -0,0 +1,71 @@
+import streamlit as st
+
+'''
+# TaxiFareModel front
+'''
+
+st.markdown('''
+Remember that there are several ways to output content into your web page...
+
+Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
+''')
+
+'''
+## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
+
+1. Let's ask for:
+- date and time
+- pickup longitude
+- pickup latitude
+- dropoff longitude
+- dropoff latitude
+- passenger count
+'''
+
+import datetime
+import requests
+
+st.subheader("🗓️ Choose your ride parameters")
+
+pickup_date = st.date_input("📅 Pickup date")
+pickup_time = st.time_input("⏰ Pickup time")
+datetime_pickup = datetime.datetime.combine(pickup_date, pickup_time)
+
+pickup_longitude = st.number_input("📍 Pickup longitude", value=-73.985428)
+pickup_latitude = st.number_input("📍 Pickup latitude", value=40.748817)
+
+dropoff_longitude = st.number_input("🏁 Dropoff longitude", value=-73.985428)
+dropoff_latitude = st.number_input("🏁 Dropoff latitude", value=40.748817)
+
+passenger_count = st.slider("👥 Passengers", min_value=1, max_value=8, value=1)
+
+st.map({
+    "lat": [pickup_latitude, dropoff_latitude],
+    "lon": [pickup_longitude, dropoff_longitude]
+})
+
+
+'''
+## Once we have these, let's call our API in order to retrieve a prediction
+
+See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
+
+🤔 How could we call our API ? Off course... The `requests` package 💡
+'''
+
+url = 'https://taxifare.lewagon.ai/predict'
+
+if url == 'https://taxifare.lewagon.ai/predict':
+
+    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
+
+'''
+
+2. Let's build a dictionary containing the parameters for our API...
+
+3. Let's call our API using the `requests` package...
+
+4. Let's retrieve the prediction from the **JSON** returned by the API...
+
+## Finally, we can display the prediction to the user
+'''
